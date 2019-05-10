d5dc6429fbd51502b64813fefb60224ddd1ea9d8
Pupperbot <pupperbot@protonmail.com>
2019-05-09 18:56:05 -0600

    Seller info coming back as just fat data.

--- web/src/ui/pages/item-detail/index.js
+++ web/src/ui/pages/item-detail/index.js
@@ -7,7 +7,8 @@ class ItemDetail extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         itemDetails : {}
+         itemDetails : {},
+         sellerInfo : {}
          //itemName
          //itemDesc
          //itemPrice
@@ -18,29 +19,33 @@ class ItemDetail extends Component {
    componentDidMount() {
      axiosWrapper.get(`/items/${this.props.match.params.id}`)
      .then(response => {
-       console.log("Detail: ", response)
+       console.log("Item Information from backend. ", response)
        this.setState({itemDetails : response.data.item})
      })
      .catch(err => {
-      console.log("Failure: ",err)
+      console.log("Error: ",err)
      });
      axiosWrapper.get('/sellers')
      .then(response => {
-       console.log('NEW BACKEND: ', response )
+       console.log('Seller Information from backend: ', response )
+       this.setState({sellerInfo: response.data.seller[0]})
      })
      .catch(err => {
-       console.log('U MESSED UP: ',err)
+       console.log('Error: ',err)
      })
    }
+
   render() {
+    console.log('meme:' ,this.state)
      const { itemDetails } = this.state;
-    return (
+     const { sellerInfo } = this.state;
+   return (
       <div styleName = 'left-desc'>
          <p styleName = 'maintag'>{itemDetails.itemName}</p>
          <p styleName = 'desctag'>Price: {itemDetails.itemPrice}</p>
          <p styleName = 'desctag'>Description: {itemDetails.itemDesc}</p>
          <p styleName = 'desctag'>Ratings: {itemDetails.ratings}</p>
-         <p styleName = 'desctag'>Seller: </p>
+         <p styleName = 'desctag'>Seller: {sellerInfo.sellerUsername} </p>
       </div> 
     );
   }
